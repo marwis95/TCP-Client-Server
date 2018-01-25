7773c730e424adf86f3de17d37111a698a165509
marwis95 <marwis95@o2.pl>
2018-01-25 08:41:12 +0100

    Client

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
@@ -13,42 +14,37 @@ namespace Client
         {
             try
             {
-                IPAddress ipAd = IPAddress.Parse("192.168.27.133");
-                // use local m/c IP address, and 
-                // use the same in the client
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
 
-                /* Initializes the Listener */
-                TcpListener myList = new TcpListener(ipAd, 8001);
+                tcpclnt.Connect("192.168.27.133", 8001);
+                // use the ipaddress as in the server program
 
-                /* Start Listeneting at the specified port */
-                myList.Start();
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
 
-                Console.WriteLine("The server is running at port 8001...");
-                Console.WriteLine("The local End point is  :" +
-                                  myList.LocalEndpoint);
-                Console.WriteLine("Waiting for a connection.....");
-
-                Socket s = myList.AcceptSocket();
-                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-
-                byte[] b = new byte[100];
-                int k = s.Receive(b);
-                Console.WriteLine("Recieved...");
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(b[i]));
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
 
                 ASCIIEncoding asen = new ASCIIEncoding();
-                s.Send(asen.GetBytes("The string was recieved by the server."));
-                Console.WriteLine("\nSent Acknowledgement");
-                /* clean up */
-                s.Close();
-                myList.Stop();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
 
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
             }
+
             catch (Exception e)
             {
                 Console.WriteLine("Error..... " + e.StackTrace);
-            }    
+            }
         }
     }
 }
--- Server/Client/Properties/AssemblyInfo.cs
+++ Server/Client/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0dc990dc-0200-4468-bf33-36f023f39fd9")]
+[assembly: Guid("266fdd40-851b-41a2-800b-dd6fef9772ae")]
 
 // Version information for an assembly consists of the following four values:
 //
